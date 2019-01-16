6a8096844a033a58b613a2001445d9c20f43ce03
Hossein Safaie <a@as-iMac.local>
2019-01-16 14:41:43 +0330

    first commit

---
+++ ValidatorService.swift
@@ -0,0 +1,64 @@
+//
+//  ValidatorService.swift
+//  Mobile Pos
+//
+//  Created by Hossein Safaie on 10/13/1397 AP.
+//  Copyright © 1397 RayanParsi. All rights reserved.
+//
+
+import Foundation
+
+class ValidatorService {
+    
+    var text: String
+    private var valid: Bool = true
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    public func required() -> ValidatorService{
+        if isValid() {
+            if text.isEmpty {
+                valid = false
+            }
+        }
+        return self
+    }
+    
+    public func minLength(count: Int) -> ValidatorService {
+        if isValid() {
+            if text.count < count {
+                valid = false
+            }
+        }
+        return self
+    }
+    
+    public func mobile() -> ValidatorService {
+        numberal()
+        if isValid() {
+            let chars = Array(text)
+            if chars.count >= 2 {
+                if chars[0] != "0" || chars[1] != "9" {
+                    valid = false
+                }
+            }
+        }
+        return self
+    }
+    
+    public func numberal() -> ValidatorService{
+        if isValid() {
+            if let _ = Int(text) {
+                return self
+            }
+            valid = false
+        }
+        return self
+    }
+    
+    public func isValid() -> Bool{
+        return valid
+    }
+}
