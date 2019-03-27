9a61eb3c062099cd013f25ac224bd8864db569d5
Ali Salmani <salmaniali1988@gmail.com>
2019-03-27 19:36:12 +0430

    email validator aded

--- ValidatorService.swift
+++ ValidatorService.swift
@@ -58,6 +58,15 @@ class ValidatorService {
         return self
     }
     
+    public func email() -> ValidatorService {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        if !emailTest.evaluate(with: text) {
+            valid = false
+        }
+        return self
+    }
+    
     public func isValid() -> Bool{
         return valid
     }
